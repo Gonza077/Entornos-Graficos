44c8856e23f0729b92071e31eb89897ebc8c2887
Gonza - PC <gonzalo07.xc@gmail.com>
2021-06-03 15:25:16 -0300

    Creacion de la pagina 404

---
+++ TPI/error404.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="./error404.css">
+    <title>Error 404</title>
+</head>
+    <body class="body">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="plantilla-error">
+                        <h1>Oops!</h1>
+                        <h2>404- Page not Found </h2>
+                        <div class="detalles-error">
+                            <strong>Disculpa, un error ha ocurrido, la pagina solicitada no se ha encontrado!</strong>
+                        </div>
+                        <div class="botones">
+                            <!-- Aca irian los href del Home y el contacto-->
+                            <a href="./Home" class="btn btn-primary btn-lg">Home</a>
+                            <a href="./Contacto" class="btn btn-info btn-lg">Contactar al soporte</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
