46949a2c5534739f03cbc6ecc0a3abd4440dc689
Franco Pinacca <francoo_27@hotmail.com>
2021-06-03 19:44:09 -0300

    Cambios a error404 para que funcione con el nuevo navbar y footer

--- TPI/error404.php
+++ TPI/error404.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="./error404.css">
-    <title>Error 404</title>
-</head>
-    <body class="body">
+<?php require('navbar.php'); ?>
+    <head>
+        <link rel="stylesheet" href="./error404.css">
+    </head>
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
@@ -28,4 +21,5 @@
             </div>
         </div>
     </body>
+    <?php require('footer.php'); ?>
 </html>
---
+++ TPI/home.php
@@ -0,0 +1,4 @@
+<?php require('navbar.php'); ?>
+ESTO ES HOME
+
+<?php require('footer.php'); ?>
