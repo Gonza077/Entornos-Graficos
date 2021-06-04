c01be667cc7f40b374784e8ea0d83046fcf41b22
David <davoulla@gmail.com>
2021-06-04 15:41:31 -0300

    Agrege unas cosas al login

--- TPI/login.php
+++ TPI/login.php
@@ -3,17 +3,25 @@
        <div class="row justify-content-center">
           <div class="col-6 col-sm-6">
             <form class="form-signin">
-              <h1 class="d-flex justify-content-center">Iniciar sesion</h1>
-              <label for="inputEmail" class="sr-only">Direccion de email</label>
-              <input type="email" id="inputEmail" class="form-control" placeholder="Direccion de email" required autofocus>
-              <label for="inputPassword" class="sr-only">Contraseña</label>
-              <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
-                <div class="checkbox mb-3">
-                  <label>
-                    <input type="checkbox" value="remember-me"> Recordarme
-                  </label>
-                </div>
+              <div class="form-group">
+                <h1 class="d-flex justify-content-center">Iniciar sesion</h1>
+                <label for="inputEmail" class="sr-only">Direccion de email</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Direccion de email" required autofocus>
+                <label for="inputPassword" class="sr-only">Contraseña</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+                  <div class="checkbox mb-3">
+                    <label>
+                      <input type="checkbox" value="remember-me"> Recordarme
+                    </label>
+                  </div>
+              </div>
+
               <button class="btn btn-lg btn-primary btn-block" type="submit">Ingresar</button>
+              <br>
+              <a href="./home.php" class="d-flex justify-content-center">¿Olvidaste tu contraseña?</a>
+              <hr>
+              <a href="./registro.php" class="btn btn-primary btn-lg btn-block">Registrarme</a>
+ 
             </form>   
           </div>
         </div>
