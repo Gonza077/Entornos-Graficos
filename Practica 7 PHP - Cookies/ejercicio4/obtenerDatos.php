de958650f6d8deb8230166e4287f6055257d6eea
DavidUlla <81929049+DavidUlla@users.noreply.github.com>
2021-06-26 20:08:23 -0300

    Delete Practica 7 PHP - Cookies/ejercicio4 directory

--- Practica 7 PHP - Cookies/ejercicio4/obtenerDatos.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-
-    $nombre = $_SESSION["nombreUsuario"] ;
-    $password = $_SESSION["password"];
-
-    echo "<h2>Ingreso el nombre de usuario: $nombre y la contraseña: $password</h2>";
-?>
