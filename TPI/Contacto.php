bf48b27b39751d2c5756f0b4d8b393698d5a9a40
Gonza077 <gonzalo07.xc@gmail.com>
2021-07-02 17:27:42 -0300

    Modificacion de Contacto para pasar por el validador

--- TPI/Contacto.php
+++ TPI/Contacto.php
@@ -5,10 +5,11 @@
     $user = $user_session->getCurrentUser();
 ?> 
 <!DOCTYPE html>
-<html>
+<html lang="es">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Contacto</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.js"></script>
     </head>
