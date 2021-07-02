3c8c4a361b26f614c73efd5e45b32889eb7c55cc
Gonza077 <gonzalo07.xc@gmail.com>
2021-07-02 19:00:47 -0300

    Verificacion de pagina de Login

--- TPI/login.php
+++ TPI/login.php
@@ -27,6 +27,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.js"></script>
   </head>
@@ -70,8 +71,6 @@
       </div>
     </div>
   </div>
+  <?php require('footer.php'); ?>
 </body>
-<?php require('footer.php'); ?>
-
-
 </html>
