de23e21809d07249f4084bfdd21f8a6bae316f15
Gonza077 <gonzalo07.xc@gmail.com>
2021-07-12 14:38:12 -0300

    Ya sube el excel con los horarios durante todo el año

--- TPI/ajax/uploadConsultas.php
+++ TPI/ajax/uploadConsultas.php
@@ -2,13 +2,12 @@
 include('../includes/db.php');
 include('../vendor/autoload.php');
 include('../includes/consulta.php');
-$consultasArr = array();
 try{
-        $allowedFileType = ['application/vnd.ms-excel','text/xls','text/xlsx','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
-        if(in_array($_FILES["fileToUpload"]["type"],$allowedFileType)){ 
+    $db =  new DB();
+    $allowedFileType = ['application/vnd.ms-excel','text/xls','text/xlsx','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+    if(in_array($_FILES["fileToUpload"]["type"],$allowedFileType)){ 
         $target_file = "../uploads/".$_FILES["fileToUpload"]["name"];
         if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$target_file)){   
-            $db =  new DB();
             $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx') -> setReadDataOnly(true);
             $hojaDeCalculo = $reader-> load ($target_file); // Cargar hoja de Excel
             $hojaDeTrabajo = $hojaDeCalculo->getActiveSheet();
@@ -17,81 +16,46 @@ try{
             $columnaMasGrande = PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($columnaMasGrande); // e.g. 5
             $lines = $filaMasGrande - 1; //Cantidad de lineas, sacando la cabecera
             if ($lines <= 0) {
-                echo('No hay datos en la hoja de Excel');
+                exit('No hay datos en la hoja de Excel');
             }
             else{
-                $startDate = date("Y")."-01-01";
-                $endDate = date("Y")."-12-31";
-                if($registros = $db-> connect() -> query("SELECT CAST(fecha AS DATE) fecha from fechas WHERE fecha = current_date()")){
-                    $row = $registros->fetch_assoc();
-                    if (!isset($row)) {
-                        $db-> connect() -> query("CALL filldates('$startDate','$endDate')");
-                    }
-                }
-                //Se saltea la primer fila y segundapor ser la cabecera y el nombre de la columna
+                $consultasArr = array();
+                //Se saltea la primer fila por ser la cabecera de la hoja de excel
                 for ($fila = 2; $fila <= $filaMasGrande; ++$fila) {
                     $consulta = new Consulta();
                     $consulta->codigo_materia =  $hojaDeTrabajo-> getCellByColumnAndRow (1, $fila) -> getValue ();
                     $consulta->legajo_profesor = $hojaDeTrabajo-> getCellByColumnAndRow (2,$fila) -> getValue ();
-                    $consulta->dia =  strtoupper($hojaDeTrabajo-> getCellByColumnAndRow (3, $fila) -> getValue());
-                    $consulta->horario_hora =  $hojaDeTrabajo-> getCellByColumnAndRow (4, $fila) -> getValue();
-                    $consulta->horario_minuto =  $hojaDeTrabajo-> getCellByColumnAndRow (5, $fila) -> getValue();
-                    //$consulta->horario = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($horario) -> format('H:i:s');
-                    $consulta->cupo = $hojaDeTrabajo-> getCellByColumnAndRow (6, $fila) -> getValue();
-                    // $sql = "INSERT INTO consulta (docente_id,materia_id,fecha,cupo) values ('$legajo','$materiaID','$fecha','$cupo')";
-                    // $db-> connect() -> query($sql);
-                    $consultasArr[] = $consulta;
-                // echo "Se cargaron correctamente los registro";
+                    $consulta->fecha = $hojaDeTrabajo -> getCellByColumnAndRow(3,$fila) ->getValue();
+                    $consulta->fecha = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($consulta->fecha) ->format("Y/m/d H:i:s");
+                    $consulta->cupo = $hojaDeTrabajo-> getCellByColumnAndRow (4, $fila) -> getValue();
+                    $consultasArr[] = $consulta;                                     
                 }
+                #Hasta aca se cargan el arreglo con las consultas que posteriormente deberan incluirse en la BD
             }
-            $dateQuery="SELECT CAST(fecha AS DATE) fecha ,WEEKDAY(fecha) as dia FROM consultas_db.fechas WHERE WEEKDAY(fecha) not in(5,6) AND fecha >= current_date() ORDER BY fecha ASC";
-            if($registros = $db-> connect() ->query($dateQuery)){
-                while ($row = $registros->fetch_assoc()) {
-                    $current_dia = $row["dia"];
-                    $current_fecha = $row["fecha"];
-                    foreach ($consultasArr as &$consulta) {
-                        if (getDayNumber($consulta->dia) == $current_dia) {
-                            $full_date = $current_fecha." ".$consulta->horario_hora.":".$consulta->horario_minuto.":00";
-                            $query = "CALL crear_consulta_excel('$consulta->codigo_materia',$consulta->legajo_profesor,'$full_date',$consulta->cupo)";
-                            $db-> connect() -> query($query);
-                        }
-                    }
+            $endDate = date("Y",time())."-12-31"; 
+            foreach($consultasArr as $consulta){
+                $fechaConsulta = date('Y-m-d H:i:s', strtotime($consulta-> fecha));
+                while( strtotime($fechaConsulta) <= strtotime($endDate) ){                   
+                    $query = "CALL crear_consulta_excel('$consulta->codigo_materia','$consulta->legajo_profesor','$fechaConsulta','$consulta->cupo')";
+                    $db -> connect() -> query($query);
+                    #Se va a repetir el proceso cada semana, durante todo el año
+                    $fechaConsulta = strtotime('+7 days',strtotime($fechaConsulta)); #Devuelve un timeStap, hay que castearlo a DateTime
+                    $fechaConsulta = date('Y-m-d H:i:s', $fechaConsulta);
                 }
             }
             http_response_code(200);
             echo json_encode("Consultas cargadas exitosamente");
         }
     }
-        else{
-            http_response_code(204);
-            echo json_encode("Formato ingresado incorrecto");
-        }
+    else{
+        http_response_code(204);
+        echo json_encode("Formato ingresado incorrecto");
+    }
 }
 catch(Exception $e){
     throw $e ->getMessage();
 }
 finally{
     $db -> disconnect();
-}
-
-
-function getDayNumber($day) {
-    switch (strtoupper($day)) {
-        case "LUNES":
-            return 0;
-            break;
-        case "MARTES":
-            return 1;
-            break;
-        case "MIERCOLES":
-            return 2;
-            break;
-        case "JUEVES":
-            return 3;
-            break;
-        case "VIERNES":
-            return 4;
-            break;
-    }
 }
 ?>
