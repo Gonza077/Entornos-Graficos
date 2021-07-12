d586569d5549a16ecc2d8ba34b81b9e413653b84
Franco Pinacca <francoo_27@hotmail.com>
2021-07-12 14:57:33 -0300

    Vista mis solicitudes

---
+++ TPI/misSolicitudes.php
@@ -0,0 +1,68 @@
+<?php 
+  include_once('includes/user_session.php');
+  include_once('includes/user.php');
+  $user_session = new UserSession();
+  $USER = $user_session->getCurrentUser();
+  if(isset($USER)){
+    if($USER->isDocente()){
+      header("Location: http://localhost/consultas.php");
+    }
+  }
+?> 
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.js"></script>
+    <title>Mis Solicitudes</title>
+</head>
+<body>
+<?php include('navbar.php'); ?>
+  <div class="container-fluid">
+    <br>
+    <br>
+    <table class="table table-hover" id="solicitudesTable" style="margin-bottom: 50px;"> 
+      <thead>
+        <tr>
+          <th scope="col">Estado</th>
+          <th scope="col">Materia</th>
+          <th scope="col">Docente</th>
+          <th scope="col">Horario</th>
+          <th scope="col">Operaciones</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+  </div>
+<!-- Cancelar Consulta Modal -->
+<div class="modal fade" id="cancelarConsultaModal" tabindex="-1" aria-labelledby="cancelarConsultaModalLabel">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+      <input type="text" name="idCancelarConsulta" id="idCancelarConsulta" hidden>
+        <h5 class="modal-title" id="cancelarConsultaModalLabel">Cancelar Consulta</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Esta seguro que desea cancelar la inscripcion a 
+        <br>
+        <b id="datosCancelarConsulta"></b>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" aria-label="Volver">Volver</button>
+        <button type="button" class="btn btn-danger" aria-label="Cancelar" id="cancelarConsulta">Cancelar</button>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+<?php include('footer.php');?>
+<script src="js/solicitudScript.js"></script>
+<?php include('components/toast.php');?>
+</html>
+
