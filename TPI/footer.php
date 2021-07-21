8cfc1b2cb3b2b72f52455b5d3e911836d77533df
Franco Pinacca <francoo_27@hotmail.com>
2021-07-21 20:23:08 -0300

    fix footer , agregar quienes somos

--- TPI/footer.php
+++ TPI/footer.php
@@ -1,6 +1,4 @@
-<!-- Footer -->
 <footer>
-    <!-- Footer Links -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
@@ -9,69 +7,36 @@
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <link rel="stylesheet" href="./css/toast.css">
-    <!-- Footer Links -->
 
-  <div class="container text-center text-md-left mt-5">
-      <!-- Grid row -->
-      <div class="row form-group">
-
-        <!-- Grid column -->
-        <div class=" col-md-4 col-lg-6 col-xl-3 mx-auto mb-4">
-          <!-- Content -->
-          <h6 class="text-uppercase font-weight-bold">Quienes somos?</h6>
-          <hr style="border-top: 3px solid gray;">
-          <p>Somos el Grupo N°4 de la materia de Entornos Graficos
-            de la UTN dictada durante el año 2021, primer cuatrimestre. 
-            <br>
-            Integrantes:</p>
-          <p>Pinacca, Franco <br>
-            Diez, Juan Pablo<br>
-            Ulla, David<br>
-            Mateucci, Nicolas<br>
-            <a target="_blank" href="https://phantom-marca.unidadeditorial.es/4a48d118c4427fc01575ac7e16d4b4a0/crop/0x70/1022x644/resize/1320/f/jpg/assets/multimedia/imagenes/2021/07/11/16259717481572.jpg" style="color:white;">Castillo, Gonzalo</a><br>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class=" col-md-4 col-lg-4 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
+  <div class="container text-center text-md-left">
+      <div class="row">
+        <div class=" col-md-4 col-lg-4 col-xl-2 mx-auto">
           <h6 class="text-uppercase font-weight-bold">Links de ayuda</h6>
-          <hr  style="border-top: 3px solid gray;">
-          <p>
+          <hr  style="border-top: 1px solid gray; margin:0px;">
+          <p style="margin-bottom:0px;">
             <a style="color:white;" href="sitemap.php">Mapa de sitio</a>
           </p>
-          <p>
+          <p style="margin-bottom:0px;">
             <a style="color:white;" href="contacto.php">Contacto</a>
           </p>
-          <p>
+          <p style="margin-bottom:0px;">
             <a style="color:white;" href="preguntasFrecuentes.php">Preguntas Frecuentes</a>
           </p>
-          <p>
+        </div>
+        <div class=" col-md-4 col-lg-4 col-xl-2 mx-auto">
+          <h6 class="text-uppercase font-weight-bold">Link Externos</h6>
+          <hr  style="border-top: 1px solid gray; margin:0px;">
+          <p style="margin-bottom:0px;">
             <a style="color:white;" target="_blank" href="https://www.alumnos.frro.utn.edu.ar/loginAlumno.asp">SYSACAD</a>
           </p>
-          <p>
+          <p style="margin-bottom:0px;">
             <a style="color:white;" target="_blank" href="https://www.frro.utn.edu.ar/">UTN FRRo</a>
           </p>
         </div>
-        <!-- Grid column -->
-
-        <div class=" col-md-4 col-lg-4 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase font-weight-bold">Fecha de publicacion</h6>
-          <hr  style="border-top: 3px solid gray;">
-          <p>
-            <?php echo date("d-m-Y H:i:s"); ?>
-          </p>  
-        </div>
   </div>
-  <!-- Grid row -->
-
-        <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2021 Copyright:
-    <a href="#"> Grupo N°4</a>
+  <div class="footer-copyright text-center">© 2021 Copyright:
+    <a href="quienesSomos.php"> Grupo N°4</a>
   </div>
-  <!-- Copyright -->
-
   </div>
 </footer>
-<!-- Footer -->
+
---
+++ TPI/quienesSomos.php
@@ -0,0 +1,49 @@
+<?php 
+    include_once('includes/user_session.php');
+    include_once('includes/user.php');
+    $user_session = new UserSession();
+    $user = $user_session->getCurrentUser();
+?> 
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>¿Quienes somos?</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.js"></script>
+        <link rel="icon" href="./assets/Icono.ico">
+        <link rel="stylesheet" href="./css/footerRelativo.css">
+    </head>
+    <body>
+    <?php require('navbar.php'); ?> 
+        <div class="container-fluid" style="min-height: 800px;">
+            <div class="row justify-content-center form-group">
+                <div class="col-12 col-sm-6">
+                    <form class="form-contact" action="contacto.php" method="post" style="margin-bottom: 100px;">
+                        <img class="mb-4 d-block mx-auto" src="img/LogoUTN.png" alt="Logo de la Universidad Tecnológica Nacional" width="100" height="100">
+                        <h1 class="d-flex justify-content-center">¿Quienes somos?</h1>
+                        <hr>
+                        <ul>
+                        <p>
+                            Somos el Grupo N°4 de la materia de <b>Entornos Graficos </b>de la UTN dictada durante el año 2021, primer cuatrimestre. 
+                            <br><h4>Integrantes:</h4> 
+                        </p>
+                        <p> 
+                            <ul>
+                                <li>Castillo, Gonzalo<br></li>
+                                <li>Diez, Juan Pablo<br></li>
+                                <li>Pinacca, Franco <br></li>
+                                <li>Ulla, David<br></li>
+                                <li>Mateucci, Nicolas<br></li>
+                            </ul>
+                        </p>
+                            </div>
+                        </ul>
+                    </form> 
+                </div>
+            </div>
+        </div>
+        <?php require('footer.php'); ?> 
+    </body>
+</html>
